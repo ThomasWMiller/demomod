7d95a7d654a186028b33de4408d9f4cdc46b2f86
research-publishers <33678732+research-publishers@users.noreply.github.com>
2023-10-21 14:11:28 -0700

    Add files via upload

--- fileutil/fileutil.go
+++ fileutil/fileutil.go
@@ -1,1 +1,44 @@
+// fileutil package provides utilities for working with files
+package fileutil
 
+// FileCopy utility
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+// fileutil.FileCopy(sourceFile, destFile)
+// Copies sourceFile to destFile.
+// Reports copy to the console.
+// Defers and reports closing of files.
+func FileCopy(sourceFile, destFile string) error {
+	src, err := os.Open(sourceFile)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		fmt.Println("closing", sourceFile)
+		src.Close()
+	}()
+
+	dst, err := os.Create(destFile)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		fmt.Println("closing", destFile)
+		src.Close()
+	}()
+
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%s copied to %s\n", sourceFile, destFile)
+	return nil
+}
